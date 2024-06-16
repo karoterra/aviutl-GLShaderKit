7e6a5cdcc02a2a9a4210774dac87f6259eef001e
karoterra <karoterra2008@gmail.com>
2024-06-16 14:33:34 +0900

    :tada: chore: 最初のコミット

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <lua.hpp>
+
+#define GL_SHADER_KIT_API extern "C" __declspec(dllexport)
+
+#ifndef GL_SHADER_KIT_VERSION
+#   define GL_SHADER_KIT_VERSION "0.0.0"
+#endif
+
+int version(lua_State* L) {
+    lua_pushstring(L, GL_SHADER_KIT_VERSION);
+    return 1;
+}
+
+static const luaL_Reg kLibFunctions[] = {
+    {"version", version},
+    {nullptr, nullptr},
+};
+
+GL_SHADER_KIT_API int luaopen_GLShaderKit(lua_State * L) {
+    luaL_register(L, "GLShaderKit", kLibFunctions);
+    return 1;
+}
